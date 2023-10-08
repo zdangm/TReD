a2f776444bdb9211f7f5c6da6d5f7d96be68f3b1
YUE XU <112360723+yuexu0312@users.noreply.github.com>
2023-10-08 12:53:42 +0800

    Update compute_d.R

--- compute_d.R
+++ compute_d.R
@@ -30,6 +30,7 @@ for (dataset_index in 1:7){
   dataset = datasets[dataset_index]
   print(dataset)
 
+  ## load TWAS
   if (dataset %in% c('blood', 'lung', 'lymphocytes', 'spleen')){
     twas_rs = as.data.frame(fread(paste0('TWAS_rs/twas_', dataset, '.txt')))
     # convert gene name into entrez ID
@@ -40,12 +41,41 @@ for (dataset_index in 1:7){
         name_index = append(name_index, i)
       })
     }
+    
     twas_rs = twas_rs[name_index,]
     twas_rs$GeneID <- mget(x=as.character(twas_rs$genename), envir=org.Hs.egALIAS2EG)
     twas_rs$GeneID <- gsub('[c()"]', '', twas_rs$GeneID) #format text of genes with multiple IDs
     twas_rs <- separate_rows(twas_rs, GeneID)
     twas_rs$padj = p.adjust(twas_rs$pvalue, method = 'fdr')
     save(twas_rs, file = paste0('TWAS_rs/twas_', dataset, '_new.Rdata')) 
+    # load(paste0('TWAS_rs/twas_', dataset, '_new.Rdata'))
+    twas_rs = as.data.frame(twas_rs)
+    
+    # remove duplicated genes
+    twas_rs = twas_rs[order(twas_rs$padj),]
+    twas_rs = twas_rs[!duplicated(twas_rs$gene),]
+    
+    twas_rs$twas_rank = rank(-twas_rs$zscore) / (dim(twas_rs)[1]) #ratio of rank
+    twas_sig_gene = subset(twas_rs, padj < 0.05, select = c('GeneID', 'twas_rank'))
+  } else{
+    ## load dge sig gene list, to reuse twas's code, so named twas_sig_gene
+    load(paste0('DGE_gene_list/', dataset, '.Rdata'))
+    dge_rs = as.data.frame(dge_rs)
+    
+    if(dataset == "BALF"){
+      gene_name_col = 'symbol'
+      fold_change_col = 'log2FoldChange'
+    } else{
+      gene_name_col = 'GeneName'
+      fold_change_col = 'log2FoldChange'
+    }
+    
+    #remove duplicated genes
+    dge_rs = dge_rs[order(abs(dge_rs[,fold_change_col]), decreasing = T),]
+    dge_rs = dge_rs[!duplicated(dge_rs[,gene_name_col]),]
+        
+    twas_sig_gene = subset(dge_rs, select = c('GeneID', 'dge_rank'))
+    colnames(twas_sig_gene)[2] = 'twas_rank'
   }
    
   
