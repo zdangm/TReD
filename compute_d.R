0fa943263262c61e28b5a423802e93046c0611cf
YUE XU <112360723+yuexu0312@users.noreply.github.com>
2023-10-08 12:28:58 +0800

    Update compute_d.R

--- compute_d.R
+++ compute_d.R
@@ -1,1 +1,17 @@
+options(warn = -1)
+
+work_dir = '~/TReS/'
+setwd(work_dir)
+
+library(AnnotationDbi)
+library(org.Hs.eg.db)
+library(tidyr)
+library(data.table)
+library(hdf5r)
+library(dplyr)
+library(foreach)
+library(doMC)
+registerDoMC(cores=max(detectCores() - 1, 1))
+
+
 
