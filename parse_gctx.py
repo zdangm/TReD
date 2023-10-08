91d2fb8008c45ab2009275051985d641e1eba661
YUE XU <112360723+yuexu0312@users.noreply.github.com>
2023-10-08 16:54:04 +0800

    Update parse_gctx.py

--- parse_gctx.py
+++ parse_gctx.py
@@ -1,1 +1,22 @@
+from cmapPy.pandasGEXpress.parse import parse
+import pandas as pd
+import numpy as np
+import os
+os.chdir('TReS/data/LINCS2_beta/')
 
+gctx_file = "level5_beta_trt_cp_n720216x12328.gctx"
+print('start parsing')
+gctx_data = parse(gctx_file)
+print('parsing is ok')
+# Access the expression matrix
+exp_mat = gctx_data.data_df
+exp_mat = pd.DataFrame(exp_mat)
+
+# Access the sample information
+pd.DataFrame(exp_mat.columns).to_csv('col.csv', index = 0, sep = '\t')
+# Access the row (gene) information
+pd.DataFrame(exp_mat.index).to_csv('row.csv', index = 0, sep = '\t')
+
+exp_mat.to_csv('exp_mat.csv', index = 0, sep = ',')
+
+print('ok')
