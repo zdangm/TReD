670da651a76ac3789377de6adb3a7453a47212c6
YUE XU <112360723+yuexu0312@users.noreply.github.com>
2023-10-08 13:11:46 +0800

    Update get_input.R

--- get_input.R
+++ get_input.R
@@ -25,9 +25,43 @@ for (dataset in datasets){
     twas_rs <- separate_rows(twas_rs, GeneID)
     twas_rs$padj = p.adjust(twas_rs$pvalue, method = 'fdr')
     save(twas_rs, file = paste0('TWAS_rs/twas_', dataset, '_new.Rdata'))
-  }
-  
-  
+  } else if (dataset == "ALV"){
+    dz_signature <- read.csv("/DGE_rs/ALV_DE.csv")
+    dz_signature = na.omit(dz_signature)
+    dz_signature$dge_rank = rank(-dz_signature$log2FoldChange) / length(dz_signature$log2FoldChange)
+    dz_signature <- dz_signature[which(dz_signature$padj < 0.05),]
+    dz_signature <- dz_signature[order(dz_signature$log2FoldChange),]
+    dz_signature = dz_signature[-which(dz_signature$GeneName == 'LOC284454'),]
+    dz_signature$GeneID <- mget(x=as.character(dz_signature$GeneName), envir=org.Hs.egALIAS2EG) #convert to entrez ID
+    dz_signature$GeneID <- gsub('[c()"]', '', dz_signature$GeneID) #format text of genes with multiple IDs
+    dz_signature <- separate_rows(dz_signature, GeneID) #genes with multiple IDs, map to all of them as individual rows
+    dge_rs = dz_signature
+    save(dge_rs, file = paste0('DGE_rs/', dataset, '_new.Rdata'))
+  } else if (dataset == "EXP"){
+    dz_signature <- read.csv("/DGE_rs/EXP_DE.csv")
+    colnames(dz_signature)[c(4,10)] <- c("log2FoldChange", "GeneID") #Relabel columns
+    dz_signature = na.omit(dz_signature)
+    dz_signature$dge_rank = rank(-dz_signature$log2FoldChange) / length(dz_signature$log2FoldChange)
+    dz_signature <- dz_signature[which(dz_signature$padj < 0.05),]
+    dz_signature <- dz_signature[which(abs(dz_signature$log2FoldChange) > 2),]
+    dz_signature <- dz_signature[order(dz_signature$log2FoldChange),]
+    dz_signature <- dz_signature[!is.na(dz_signature$GeneID),] # keep genes with valid entrez ID
+    dz_signature <- dz_signature[which(dz_signature$GeneID %in% gene_list$V1),] # keep genes in cmap
+    dge_rs = dz_signature
+    save(dge_rs, file = paste0('DGE_rs/', dataset, '_new.Rdata'))
+  } else if (dataset == "BALF"){
+    dz_signature <- read.csv("/DGE_rs/BALF_DE.csv")
+    colnames(dz_signature)[c(4,9)] <- c("log2FoldChange", "GeneID") #Relabel columns: estimate is log2FC
+    dz_signature = na.omit(dz_signature)
+    dz_signature$dge_rank = rank(-dz_signature$log2FoldChange) / length(dz_signature$log2FoldChange)
+    dz_signature <- dz_signature[which(dz_signature$p.adjusted < 0.05),]
+    dz_signature <- dz_signature[which(abs(dz_signature$log2FoldChange) > 4),]
+    dz_signature <- dz_signature[order(dz_signature$log2FoldChange),]
+    dz_signature <- dz_signature[!is.na(dz_signature$GeneID),] # keep genes with valid entrez ID
+    dz_signature <- dz_signature[which(dz_signature$GeneID %in% gene_list$V1),] # keep genes in cmap
+    dge_rs = dz_signature
+    save(dge_rs, file = paste0('DGE_rs/', dataset, '_new.Rdata'))
+  }  
 }
 
 
