05e82e4620626787e4c7508dcf95561d95874609
YUE XU <112360723+yuexu0312@users.noreply.github.com>
2023-10-08 12:35:56 +0800

    Update compute_d.R

--- compute_d.R
+++ compute_d.R
@@ -13,5 +13,15 @@ library(foreach)
 library(doMC)
 registerDoMC(cores=max(detectCores() - 1, 1))
 
+### get data
+## load compound signatures
+gene_list = as.data.frame(fread('row.csv'))
+compound_signatures = as.data.frame(fread('compound_signatures/exp_mat.csv'))
+row.names(compound_signatures) = gene_list$rid
 
+## load TWAS results and DGE results
+args = as.numeric(commandArgs(TRUE)) #which dataset
+datasets = c('blood', 'lung', 'lymphocytes', 'spleen', 'ALV', 'EXP', 'BALF')
+dataset = datasets[args]
+twas_rs = as.data.frame(fread(paste0('TWAS_rs/twas_', dataset, '.txt')))
 
